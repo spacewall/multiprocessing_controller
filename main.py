e79ca15cac8b0bbf21b073815908feb34d85fa9a
Alexander Godov <godovav@oiate.ru>
2024-08-15 17:37:11 +0300

    Create an async executor function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from time import time, sleep
 from multiprocessing import Process, cpu_count
+from types import coroutine
 from typing import Callable, Tuple, List, Any
 
 FunctionType = Callable[..., Any]
@@ -17,6 +20,18 @@ class ProcessController:
 
     def __str__(self) -> str:
         return f"Qeue controller, {cpu_count()} CPUs are available on your machine"
+    
+    async def run_in_executor(self, executor: ThreadPoolExecutor, task_number: int, task_function: function, timeout: int) -> coroutine:
+        loop = asyncio.get_event_loop()
+        try:
+            result = await asyncio.wait_for(loop.run_in_executor(executor, task_function, task_number), timeout=timeout)
+
+            return result
+
+        except asyncio.TimeoutError:
+            print(f"The task #{task_number} time limit has been exceeded")
+
+            return None
 
     def set_max_proc(self, n: int) -> None:
         """:param n: proc number"""
@@ -30,8 +45,21 @@ class ProcessController:
             self.max_proc = n
             print(f"{n} CPUs are installed")
 
-    def start(self, tasks: TaskListType, max_exec_time):
-        pass
+    def start(self, tasks: TaskListType, max_exec_time: int):
+        """
+        :param tasks: list of tasks like [(function0, (f0_arg0, f0_arg1)), 
+        (function1, (f1_arg0, f1_arg1, f1_arg2)), (function2, (f2_arg0, ...)), ...]
+        :param max_exec_time: max task execution time in seconds
+        """
+
+        if self.max_proc is None:
+            max_workers = cpu_count()
+
+        else:
+            max_workers = self.max_proc
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            pass
 
     def wait(self):
         pass
@@ -51,6 +79,9 @@ def test():
 
     if stop - start > 1:
         print("Success!")
+
+async def main():
+    pass
 
 if __name__ == "__main__":
     process = Process(target=test)
